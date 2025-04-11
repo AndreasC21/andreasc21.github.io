854bdda4e75df5dfdac33fb8c2204285ecc22015
AndreasC <andreascalvin21@gmail.com>
2025-04-11 22:26:40 +0700

    update style, design, and many things

--- js/script.js
+++ js/script.js
@@ -1,4 +1,33 @@
-document.addEventListener("DOMContentLoaded", function () {
-  const homeSection = document.querySelector(".home");
-  homeSection.classList.add("animate");
+let scrolling = 0;
+let scrollFlag = 1;
+
+function scrollEvent(event) {
+  console.log(event);
+  if (event.deltaY < 0) {
+    console.log("deltay");
+    if (scrolling !== 0) {
+      console.log("scrolling");
+      scrolling += 100;
+      document.getElementById(
+        "wrapper"
+      ).style.transform = `translateY(${scrolling}vh)`;
+    }
+  } else if (event.deltaY > 0) {
+    if (scrolling > -300) {
+      scrolling -= 100;
+      document.getElementById(
+        "wrapper"
+      ).style.transform = `translateY(${scrolling}vh)`;
+    }
+  }
+}
+
+window.addEventListener("wheel", function (event) {
+  if (scrollFlag === 1) {
+    setTimeout(() => {
+      scrollEvent(event);
+      scrollFlag = 1;
+    }, 300);
+    scrollFlag = 0;
+  }
 });
