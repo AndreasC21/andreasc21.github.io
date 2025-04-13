780a2df6cfa43539e5b4424ea3ffc05a1aade887
AndreasC <andreascalvin21@gmail.com>
2025-04-13 15:41:56 +0700

    change to scroll snap animation instead scrollevent, fix scroll on mobile devices

--- js/script.js
+++ js/script.js
@@ -1,33 +1,9 @@
-let scrolling = 0;
-let scrollFlag = 1;
-
-function scrollEvent(event) {
-  console.log(event);
-  if (event.deltaY < 0) {
-    console.log("deltay");
-    if (scrolling !== 0) {
-      console.log("scrolling");
-      scrolling += 100;
-      document.getElementById(
-        "wrapper"
-      ).style.transform = `translateY(${scrolling}vh)`;
-    }
-  } else if (event.deltaY > 0) {
-    if (scrolling > -300) {
-      scrolling -= 100;
-      document.getElementById(
-        "wrapper"
-      ).style.transform = `translateY(${scrolling}vh)`;
-    }
-  }
+function openNavMobile() {
+  const nav = document.querySelector(".nav-mobile");
+  nav.style.display = "flex";
 }
 
-window.addEventListener("wheel", function (event) {
-  if (scrollFlag === 1) {
-    setTimeout(() => {
-      scrollEvent(event);
-      scrollFlag = 1;
-    }, 300);
-    scrollFlag = 0;
-  }
-});
+function closeNavMobile() {
+  const nav = document.querySelector(".nav-mobile");
+  nav.style.display = "none";
+}
